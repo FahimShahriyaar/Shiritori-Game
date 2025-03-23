bcc70181b23563b7b7c163e16008d558abaefb74
Fahim Shahriyaar <shahriyaar.flash@gmail.com>
2025-03-23 22:32:11 +0600

    finalize

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,7 @@ function App() {
     if (isRunning[0]) {
       interval = setInterval(() => {
         setActivePlayer((prev) => prev == 1 ? 2 : 1)
+        setCurrentWord(letters[Math.floor(Math.random() * 26)])
       }, 10100);
     }
 
@@ -59,14 +60,14 @@ function App() {
   return (
     <>
       <header className=' bg-gray-950 p-4 flex justify-between items-center'>
-        <h1 className='text-white text-4xl'>Shiritori Delta</h1>
+        <h1 className='text-white text-4xl outfit-title'>Shiritori Delta</h1>
         <div className='flex gap-2'>
           <button onClick={()=>instruction[1](true)} className='text-white text-xl bg-amber-800 hover:bg-amber-700 py-1 px-2 rounded-sm'>Instructions</button>
           <button onClick={()=>about[1](true)} className='text-white text-xl bg-amber-800 hover:bg-amber-700 py-1 px-2 rounded-sm'>About</button>
         </div>
       </header>
       <main className={`${((isRunning[1]=='Play Again')||instruction[0]||about[0]) && 'blur-xs'}`}>
-        <div className='mt-10 flex justify-center gap-10'>
+        <div className='mt-10 flex items-center justify-center gap-10 flex-col md:flex-row'>
           <GamePanel PlayerLogs={PlayerLogs} NextWord={NextWord} CurrentWord={CurrentWord} setActivePlayer={setActivePlayer} ActivePlayer={ActivePlayer} pid='1' winner={winner} />
           <GamePanel PlayerLogs={PlayerLogs} NextWord={NextWord} CurrentWord={CurrentWord} setActivePlayer={setActivePlayer} ActivePlayer={ActivePlayer} pid='2' winner={winner} />
         </div>
